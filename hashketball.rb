ef2752c27b98099523d2a7d840135f894dc00091
Roman Turner <rn.turner@live.com>
2021-01-28 00:18:40 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,5 +1,5 @@
 # Write your code below game_hash
-
+require 'pry'
 def game_hash
   {
     home: {
@@ -128,3 +128,99 @@ def game_hash
 end
 
 # Write code here
+def all_players
+  all_players = game_hash[:away][:players].concat(game_hash[:home][:players])
+end
+
+def num_points_scored (name)
+  points_scored = 0
+  all_players.each do |player|
+    if player[:player_name] == name
+    points_scored = player[:points]
+  end 
+end
+  points_scored
+end
+
+def shoe_size (name)
+  shoe_size = 0
+  all_players.each do |player|
+    if player[:player_name] == name
+    shoe_size = player[:shoe]
+  end 
+end
+  shoe_size
+end
+
+def team_colors (team)
+  
+case
+when team == game_hash[:home][:team_name]
+  game_hash[:home][:colors]
+when team == game_hash[:away][:team_name]
+  game_hash[:away][:colors]
+else 
+  return nil 
+end
+end 
+
+def team_names 
+  both_team_names = []
+  both_team_names << game_hash[:away][:team_name]
+  both_team_names << game_hash[:home][:team_name]
+  both_team_names
+end 
+  
+  
+  
+  def player_numbers roster
+  roster_by_number = []
+  game_hash.each do |team, team_info|
+    if team_info[:team_name] == roster
+     team_info.each do |roster, player|
+      if roster == :players
+        player.each do |stats|
+          roster_by_number.push(stats[:number])
+        end 
+       end
+      end
+    end
+  end
+  roster_by_number  
+end
+
+  
+def player_stats players_stats
+all_players.each do |players|
+  if players[:player_name] == players_stats
+    return players
+  end
+end
+end
+  
+def big_shoe_rebounds
+  shoe_size = 0
+  rebounds = 0
+  all_players.each do |player|
+  if player[:shoe] > shoe_size
+        shoe_size = player[:shoe]
+        rebounds = player[:rebounds]
+      end
+    end
+    rebounds
+end 
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
